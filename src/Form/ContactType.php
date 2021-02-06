c1d77570f0defd93e18261b703a7bd50002f3f5b
kevocde <kevocde@gmail.com>
2021-02-06 17:39:34 -0500

    Añadidas vistas y redirecciones en menú

---
+++ src/Form/ContactType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('message')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('deleted_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
