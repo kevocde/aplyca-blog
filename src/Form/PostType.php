5b77eca183ddf5dc960d8172e29beb279b1dffb5
kevocde <kevocde@gmail.com>
2021-02-06 11:33:54 -0500

    Corregidos problemas con permisos y generación CRUD para los posts

---
+++ src/Form/PostType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author_id')
+            ->add('slug')
+            ->add('title')
+            ->add('image')
+            ->add('content')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('deleted_at')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
